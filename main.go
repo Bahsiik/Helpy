3c53f4e478cc74c56ac2c95cb3552032587e7ab1
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-03-30 09:36:49 +0200

    merge 3

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ type User struct {
 func main() {
 
 	// Gestion de tous les fichiers gohtml
-	tmpl := template.Must(template.ParseGlob("templates/*.gohtml"))
+	tmpl := template.Must(template.ParseGlob("templates/login.gohtml"))
 	cssFolder := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", cssFolder))
 
@@ -77,18 +77,10 @@ func main() {
 			return
 		}
 		switch r.Method {
-		case "GET":
-			////  Recherche des albums d'un artiste défini
-			albums, err := albumsByArtist("John Coltrane")
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("Albums found: %v\n", albums)
-			tmpl.ExecuteTemplate(w, "index.gohtml", albums)
 		case "POST":
 			// Ajout d'un User
-			albID, err := addAlbum(User{
-				USERNAME: r.FormValue("username"),
+			albID, err := addUser(User{
+				USERNAME: r.FormValue("Username"),
 				PASSWORD: r.FormValue("password"),
 			})
 			if err != nil {
@@ -103,87 +95,16 @@ func main() {
 	}
 }
 
-// albumsByArtist recherche des albums d'un artiste défini
-func albumsByArtist(name string) ([]User, error) {
-	// Variable pour contenir les données renvoyées par la fonction
-	var User []User
-
+func addUser(use User) (int64, error) {
 	// Création de la requête SQL
-	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	result, err := db.Exec("INSERT INTO users (Username, Password) VALUES (?, ?)", use.USERNAME, use.PASSWORD)
 	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-	// Permet de relâcher les informations contenues dans la variable rows lorsque la fonction s'arrête
-	defer rows.Close()
-	// Boucle pour remplir le tableau []Album des données de la recherche
-	for rows.Next() {
-		var Use User
-		if err := rows.Scan(&Use.USERNAME, &Use.PASSWORD); err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-		}
-		albums = append(User, Use)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-	return User, nil
-}
-
-// albumByID recherche d'album via l'ID.
-func albumByID(id int64) (Album, error) {
-	// Variable de type album pour contenir les données du retour de la recherche
-	var alb Album
-
-	// Création de la requête SQL
-	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
-	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-		if err == sql.ErrNoRows {
-			return alb, fmt.Errorf("albumsById %d: no such album", id)
-		}
-		return alb, fmt.Errorf("albumsById %d: %v", id, err)
-	}
-	return alb, nil
-
-}
-
-// addAlbum Ajout d'un album,
-// Renvoi également l'ID du nouvel album
-
-func addAlbum(alb Album) (int64, error) {
-	// Création de la requête SQL
-	result, err := db.Exec("INSERT INTO album (Username, Password) VALUES (?, ?)", use.USERNAME, use.PASSWORD)
-	if err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
+		return 0, fmt.Errorf("addUser: %v", err)
 	}
 	// Récupération de l'ID pour le return
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
-	}
-	return id, nil
-}
-
-// delAlbum suppression d'un album voulu,
-// Renvoi également le nom de l'album supprimé
-func delAlbum(alb Album) (string, error) {
-	// Récupération du nom pour le return
-	albTitle := alb.Title
-	// Création de la requête SQL
-	_, err := db.Exec("DELETE FROM album WHERE title=(?)", alb.Title)
-	if err != nil {
-		return "", fmt.Errorf("delAlbum: %v", err)
-	}
-	return albTitle, nil
-}
-func ModAlbum(alb Album) (int64, error) {
-
-	result, err := db.Exec("UPDATE album SET id= ?, title= ?, artist= ?, price =? WHERE id = ?", alb.ID, alb.Title, alb.Artist, alb.Price, alb.ID)
-	if err != nil {
-		return 0, fmt.Errorf("ModAlbum: %v", err)
-	}
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, fmt.Errorf("ModAlbum: %v", err)
+		return 0, fmt.Errorf("addUser: %v", err)
 	}
 	return id, nil
 }
