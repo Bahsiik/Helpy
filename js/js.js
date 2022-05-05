b0a5c34de486910311ff2cfac408e6a31d40ec05
Davy <davy.marthely@ynov.com>
2022-05-05 12:10:18 +0200

    Page principale

---
+++ js/js.js
@@ -0,0 +1,41 @@
+function search_topics() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('topics');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+}
+
+document.querySelectorAll('.like').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.src === "http://localhost:63342/Forum/image/coeur%20(1).png") {  // to change
+            item.src = "../image/coeur.png"
+            item.nextElementSibling.innerHTML = parseInt(item.nextElementSibling.innerHTML) - 1
+            return
+        }
+        item.src = "../image/coeur (1).png"
+
+        item.nextElementSibling.nextElementSibling.src = "../image/etoile.png"
+        item.nextElementSibling.innerHTML = parseInt(item.nextElementSibling.innerHTML) + 1
+    })
+})
+
+document.querySelectorAll('.fav').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.src == "http://localhost:63342/Forum/image/etoile%20(1).png") { // to change
+            item.src = "../image/etoile.png"
+            item.previousElementSibling.innerHTML = parseInt(item.previousElementSibling.innerHTML) + 1
+            return
+        }
+        item.src = "../image/etoile (1).png"
+        item.previousElementSibling.previousElementSibling.src = "../images/coeur.png"
+        item.previousElementSibling.innerHTML = parseInt(item.previousElementSibling.innerHTML) - 1
+    })
+})
