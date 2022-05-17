495ed55482a2a48e3c1cc17c51f9d9a693f841af
Davy <davy.marthely@ynov.com>
2022-05-17 17:43:22 +0200

    Finition param

--- Dany/js/js.js
+++ Dany/js/js.js
@@ -35,21 +35,45 @@ document.querySelectorAll('.fav').forEach(item => {
 
 function mail() {
     document.getElementById('e-mail').innerHTML = document.getElementById('mail').value;
+    document.querySelector('#change').innerHTML = 'Changer';
+    document.querySelector('.mail2').style.display = 'none';
+    document.querySelector('#change').style.color = 'black';
 }
-
+function pseudo() {
+    document.getElementById('pseudo').innerHTML = document.getElementById('p_seudo').value;
+    document.querySelector('#change').innerHTML = 'Changer';
+    document.querySelector('.pseudo2').style.display = 'none';
+    document.querySelector('#change').style.color = 'black';
+}
 document.querySelectorAll('.modif_email').forEach(item => {
     item.addEventListener('click', event => {
-        document.querySelector('.mail2').style.display = 'block';
+        if (document.querySelector('#change').innerHTML === 'Changer'){
+            document.querySelector('#change').innerHTML = 'Annuler';
+            document.querySelector('#change').style.color = 'red';
+            document.querySelector('.mail2').style.display = 'block';
+        } else {
+            document.querySelector('#change').innerHTML = 'Changer';
+            document.querySelector('.mail2').style.display = 'none';
+            document.querySelector('#change').style.color = 'black';
+        }
     })
 })
-document.querySelectorAll('#close').forEach(item => {
+document.querySelectorAll('.modif_pseudo').forEach(item => {
     item.addEventListener('click', event => {
-        document.querySelector('.mail2').style.display = 'none';
+        if (document.querySelector('#change').innerHTML === 'Changer'){
+            document.querySelector('#change').innerHTML = 'Annuler';
+            document.querySelector('#change').style.color = 'red';
+            document.querySelector('.pseudo2').style.display = 'block';
+        } else {
+            document.querySelector('#change').innerHTML = 'Changer';
+            document.querySelector('.pseudo2').style.display = 'none';
+            document.querySelector('#change').style.color = 'black';
+        }
     })
 })
 document.querySelectorAll('.aria').forEach(item => {
     item.addEventListener('click', event => {
-        if (item.ariaChecked == 'false') {
+        if (item.ariaChecked === 'false') {
             document.querySelector('.aria').ariaChecked = 'true';
             document.querySelector('.round').style.marginLeft = '20px';
             document.querySelector('.aria').style.backgroundColor = 'black'
@@ -57,6 +81,45 @@ document.querySelectorAll('.aria').forEach(item => {
             document.querySelector('.aria').ariaChecked = 'false';
             document.querySelector('.aria').style.backgroundColor = 'white'
             document.querySelector('.round').style.marginLeft = '';
+        }
+    })
+})
+document.querySelectorAll('.aria1').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.ariaChecked === 'false') {
+            document.querySelector('.aria1').ariaChecked = 'true';
+            document.querySelector('.round1').style.marginLeft = '20px';
+            document.querySelector('.aria1').style.backgroundColor = 'black'
+        } else {
+            document.querySelector('.aria1').ariaChecked = 'false';
+            document.querySelector('.aria1').style.backgroundColor = 'white'
+            document.querySelector('.round1').style.marginLeft = '';
+        }
+    })
+})
+document.querySelectorAll('.aria2').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.ariaChecked === 'false') {
+            document.querySelector('.aria2').ariaChecked = 'true';
+            document.querySelector('.round2').style.marginLeft = '20px';
+            document.querySelector('.aria2').style.backgroundColor = 'black'
+        } else {
+            document.querySelector('.aria2').ariaChecked = 'false';
+            document.querySelector('.aria2').style.backgroundColor = 'white'
+            document.querySelector('.round2').style.marginLeft = '';
+        }
+    })
+})
+document.querySelectorAll('.aria3').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.ariaChecked === 'false') {
+            document.querySelector('.aria3').ariaChecked = 'true';
+            document.querySelector('.round3').style.marginLeft = '20px';
+            document.querySelector('.aria3').style.backgroundColor = 'black'
+        } else {
+            document.querySelector('.aria3').ariaChecked = 'false';
+            document.querySelector('.aria3').style.backgroundColor = 'white'
+            document.querySelector('.round3').style.marginLeft = '';
         }
     })
 })
