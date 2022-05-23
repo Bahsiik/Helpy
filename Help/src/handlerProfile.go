d48a3f63dd2a408b857ce47b2d0256b39d3ec0aa
Davy <davy.marthely@ynov.com>
2022-05-23 15:05:14 +0200

    Rectification

--- Help/src/handlerProfile.go
+++ Help/src/handlerProfile.go
@@ -14,39 +14,3 @@ func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func ProfileComHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** profileComHandler ***")
-	d := GetUsernameFromSession(w, r)
-	err := TMPL.ExecuteTemplate(w, "profileCom.html", d)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-func ProfileFavHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** profileFavHandler ***")
-	d := GetUsernameFromSession(w, r)
-	err := TMPL.ExecuteTemplate(w, "profileFav.html", d)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-func ProfileLikeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** profileLikeHandler ***")
-	d := GetUsernameFromSession(w, r)
-	err := TMPL.ExecuteTemplate(w, "profileLike.html", d)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-func ProfilePubHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** profilePubHandler ***")
-	d := GetUsernameFromSession(w, r)
-	err := TMPL.ExecuteTemplate(w, "profilePub.html", d)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
